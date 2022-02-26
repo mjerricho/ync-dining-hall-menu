079bb43692dfe2f50f86425e37ffb4647787cd62
Marcellinus Jerricho <marcellinus.jerricho@u.yale-nus.edu.sg>
2022-02-26 11:08:30 +0800

    used exception for the scraping to increase resilience and rewroe the craft message function to reduce repetition

--- Runner.py
+++ Runner.py
@@ -1,11 +1,15 @@
+from gc import get_stats
 from SatsScrape import SatsScrape, Day  
 from datetime import datetime
 
 if __name__ == "__main__":
     day = Day.WEEKDAY if datetime.today().weekday() <= 4 else Day.WEEKEND
+    print(f"day is {day}")
     sats_scrape = SatsScrape("chromedriver", day)
     sats_scrape.login()
     sats_scrape.get_meal_links()
     sats_scrape.get_all_meals()
     message = sats_scrape.craft_message_all()
+    print(message)
+    # only uncomment this if message is right
     sats_scrape.send_message(message)
