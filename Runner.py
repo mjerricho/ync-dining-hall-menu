22cfcfe4b2c613e9c93346f5fcb0cb64527d7b28
Marcellinus Jerricho <marcellinus.jerricho@u.yale-nus.edu.sg>
2022-03-20 16:05:05 +0800

    day

--- Runner.py
+++ Runner.py
@@ -3,7 +3,8 @@ from SatsScrape import SatsScrape, Day
 from datetime import datetime
 
 if __name__ == "__main__":
-    sats_scrape = SatsScrape("chromedriver", Day.WEEKEND)
+    day = Day.WEEKDAY if datetime.today().weekday() <= 4 else Day.WEEKEND
+    sats_scrape = SatsScrape("chromedriver", day)
     sats_scrape.login()
     # sats_scrape.get_day()
     # sats_scrape.go_tomorrow()
